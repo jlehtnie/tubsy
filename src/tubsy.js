ee867f7b414f5744919beba68781408ec7919c6d
Jukka Lehtniemi <jukka.lehtniemi@gmail.com>
2015-03-12 11:59:46 +0200

    Introduce function to add more rows

--- src/tubsy.js
+++ src/tubsy.js
@@ -1,34 +1,36 @@
-$(window).load(function(){
+$(document).ready(function() {
     $(function () {
         var squares = [];
         var SIZE = 16;
         var symbol = "X";
 
-        /*
-         * Set the clicked-on square to the current symbol
-         */
-        set = function () {
+        setSquareToSymbol = function () {
             $(this).html() ? $(this).html("") : $(this).html(symbol);
             console.log($(this));
         };
 
-        /*
-         * Create and attach the DOM elements for the board as an
-         * HTML table, assign the indicators for each cell, and start
-         */
-        run = function () {
-            var board = $("<table border=1 cellspacing=0>");
+        $.fn.addRow = function () {
             var row = $("<tr>");
-            board.append(row);
+            $(this).append(row);
             for (var i = 0; i < SIZE; i += 1) {
                 var cell = $("<td height=50 width=50 align=center valign=center></td>");
-                cell.click(set);
+                cell.click(setSquareToSymbol);
                 row.append(cell);
                 squares.push(cell);
             }
+        };
 
-            // Attach under tubsy if present, otherwise to body.
-            $(document.getElementById("tubsy") || document.body).append(board);
+        addRowToTable = function () {
+            $("table").addRow()
+        }
+
+
+        run = function () {
+            var board = $("<table border=1 cellspacing=0>");
+            board.addRow();
+            var root = $(document.getElementById("tubsy") || document.body)
+            root.append(board);
+            root.append($("<a href='#'>add row</a>").click(addRowToTable));
         };
         run();
     });
